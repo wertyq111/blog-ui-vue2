5260a6a44176ecff06c6f01c798bedf0ff63066a
wertyq111 <wertyq222@163.com>
2023-06-12 11:36:00 +0800

    修复后台分类管理页面， 新增 delete 请求

--- src/components/admin/sortList.vue
+++ src/components/admin/sortList.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <div style="margin-bottom: 20px">
-      <el-button type="primary" @click="sortDialog = true">新增分类</el-button>
+      <el-button type="primary" @click="categoryDialog = true">新增分类</el-button>
     </div>
-    <el-table :data="sortInfo" border class="table" header-cell-class-name="table-header">
+    <el-table :data="categories" border class="table" header-cell-class-name="table-header">
       <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-      <el-table-column prop="sortName" label="分类名称" align="center"></el-table-column>
-      <el-table-column prop="sortDescription" label="分类描述" align="center"></el-table-column>
+      <el-table-column prop="name" label="分类名称" align="center"></el-table-column>
+      <el-table-column prop="description" label="分类描述" align="center"></el-table-column>
       <el-table-column label="分类类型" align="center">
         <template slot-scope="scope">
-          <span v-if="scope.row.sortType === 0">导航栏分类</span>
-          <span v-else-if="scope.row.sortType === 1">普通分类</span>
+          <span v-if="scope.row.type === 0">导航栏分类</span>
+          <span v-else-if="scope.row.type === 1">普通分类</span>
         </template>
       </el-table-column>
       <el-table-column prop="priority" label="分类优先级" align="center"></el-table-column>
       <el-table-column prop="countOfSort" label="文章总数" align="center"></el-table-column>
       <el-table-column label="操作" width="380" align="center">
         <template slot-scope="scope">
-          <el-button type="text" icon="el-icon-edit" @click="editSort(scope.row)">
+          <el-button type="text" icon="el-icon-edit" @click="editCategory(scope.row)">
             编辑分类
           </el-button>
           <el-button type="text" icon="el-icon-edit" @click="sayLabel(scope.row)">
@@ -34,15 +34,15 @@
       </el-table-column>
     </el-table>
 
-    <el-table v-if="!$common.isEmpty(sort)" :data="sort.labels" border class="table"
+    <el-table v-if="!$common.isEmpty(category)" :data="category.labels" border class="table"
               style="margin-top: 40px"
               header-cell-class-name="table-header">
       <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
       <el-table-column label="分类名称" align="center">
-        <span>{{sort.sortName}}</span>
+        <span>{{ category.name }}</span>
       </el-table-column>
-      <el-table-column prop="labelName" label="标签名称" align="center"></el-table-column>
-      <el-table-column prop="labelDescription" label="标签描述" align="center"></el-table-column>
+      <el-table-column prop="name" label="标签名称" align="center"></el-table-column>
+      <el-table-column prop="description" label="标签描述" align="center"></el-table-column>
       <el-table-column prop="countOfLabel" label="文章总数" align="center"></el-table-column>
       <el-table-column label="操作" width="320" align="center">
         <template slot-scope="scope">
@@ -58,7 +58,7 @@
     </el-table>
 
     <el-dialog title="分类"
-               :visible.sync="sortDialog"
+               :visible.sync="categoryDialog"
                width="30%"
                :before-close="handleClose"
                :append-to-body="true"
@@ -66,26 +66,26 @@
                center>
       <div class="my-dialog">
         <div class="myCenter">
-          <el-radio-group v-model="sortForHttp.sortType">
+          <el-radio-group v-model="categoryForHttp.type">
             <el-radio-button :label="0">导航栏分类</el-radio-button>
             <el-radio-button :label="1">普通分类</el-radio-button>
           </el-radio-group>
         </div>
-        <el-input placeholder="请输入分类名称" v-model="sortForHttp.sortName">
+        <el-input placeholder="请输入分类名称" v-model="categoryForHttp.name">
           <template slot="prepend">分类名称</template>
         </el-input>
-        <el-input placeholder="请输入分类描述" v-model="sortForHttp.sortDescription">
+        <el-input placeholder="请输入分类描述" v-model="categoryForHttp.description">
           <template slot="prepend">分类描述</template>
         </el-input>
         <el-input type="number" placeholder="请输入整数，数字小的在前面"
-                  v-model="sortForHttp.priority">
+                  v-model="categoryForHttp.priority">
           <template slot="prepend">分类优先级</template>
         </el-input>
       </div>
 
       <span slot="footer" class="dialog-footer">
           <el-button @click="handleClose()">取 消</el-button>
-          <el-button type="primary" @click="saveSortEdit()">确 定</el-button>
+          <el-button type="primary" @click="saveEdit()">确 定</el-button>
       </span>
     </el-dialog>
 
@@ -97,10 +97,10 @@
                destroy-on-close
                center>
       <div class="my-dialog">
-        <el-input placeholder="请输入标签名称" v-model="labelForHttp.labelName">
+        <el-input placeholder="请输入标签名称" v-model="labelForHttp.name">
           <template slot="prepend">标签名称</template>
         </el-input>
-        <el-input placeholder="请输入标签描述" v-model="labelForHttp.labelDescription">
+        <el-input placeholder="请输入标签描述" v-model="labelForHttp.description">
           <template slot="prepend">标签描述</template>
         </el-input>
       </div>
@@ -114,210 +114,202 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        sortDialog: false,
-        labelDialog: false,
-        sortInfo: [],
-        sort: {},
-        sortForHttp: {
-          id: null,
-          sortName: "",
-          sortDescription: "",
-          sortType: null,
-          priority: null
-        },
-        labelForHttp: {
-          id: null,
-          sortId: null,
-          labelName: "",
-          labelDescription: ""
-        }
-      }
-    },
-
-    computed: {},
-
-    watch: {},
-
-    created() {
-      this.getSortInfo();
-    },
-
-    mounted() {
-
-    },
-
-    methods: {
-      deleteHandle(id, flag) {
-        let url;
-        if (flag === 1) {
-          url = "/webInfo/deleteSort";
-        } else if (flag === 2) {
-          url = "/webInfo/deleteLabel";
-        } else {
-          return;
-        }
-        this.$confirm('确认删除？', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'success',
-          center: true
-        }).then(() => {
-          this.$http.get(this.$constant.baseURL + url, {id: id}, true)
-            .then((res) => {
-              this.$message({
-                message: "删除成功！",
-                type: "success"
-              });
-              this.getSortInfo();
-              this.sort = {};
-            })
-            .catch((error) => {
-              this.$message({
-                message: error.message,
-                type: "error"
-              });
-            });
-        }).catch(() => {
-          this.$message({
-            type: 'success',
-            message: '已取消删除!'
-          });
-        });
+export default {
+  data() {
+    return {
+      categoryDialog: false,
+      labelDialog: false,
+      categories: [],
+      category: {},
+      categoryForHttp: {
+        id: null,
+        name: "",
+        description: "",
+        type: null,
+        priority: null
       },
-      saveSortEdit() {
-        if (this.$common.isEmpty(this.sortForHttp.sortType) ||
-          this.$common.isEmpty(this.sortForHttp.priority) ||
-          this.$common.isEmpty(this.sortForHttp.sortName) ||
-          this.$common.isEmpty(this.sortForHttp.sortDescription)) {
-          this.$message({
-            message: "请完善所有分类信息！",
-            type: "error"
-          });
-          return;
-        }
-
-        let url;
-        if (this.$common.isEmpty(this.sortForHttp.id)) {
-          url = "/webInfo/saveSort";
-        } else {
-          url = "/webInfo/updateSort";
-        }
-        this.$http.post(this.$constant.baseURL + url, this.sortForHttp, true)
-          .then((res) => {
-            this.$message({
-              message: "保存成功！",
-              type: "success"
-            });
-            this.getSortInfo();
-            this.handleClose();
-          })
-          .catch((error) => {
-            this.$message({
-              message: error.message,
-              type: "error"
-            });
-          });
-      },
-      saveLabelEdit() {
-        if (this.$common.isEmpty(this.labelForHttp.labelName) ||
-          this.$common.isEmpty(this.labelForHttp.labelDescription)) {
-          this.$message({
-            message: "请完善所有标签信息！",
-            type: "error"
-          });
-          return;
-        }
-
-        let url;
-        if (this.$common.isEmpty(this.labelForHttp.id)) {
-          url = "/webInfo/saveLabel";
-        } else {
-          url = "/webInfo/updateLabel";
-        }
-        this.$http.post(this.$constant.baseURL + url, this.labelForHttp, true)
-          .then((res) => {
-            this.$message({
-              message: "保存成功！",
-              type: "success"
-            });
-            this.getSortInfo();
-            this.handleClose();
-            this.sort = {};
-          })
-          .catch((error) => {
-            this.$message({
-              message: error.message,
-              type: "error"
-            });
-          });
-      },
-      editSort(sort) {
-        this.sortDialog = true;
-        this.sortForHttp.id = sort.id;
-        this.sortForHttp.sortName = sort.sortName;
-        this.sortForHttp.sortDescription = sort.sortDescription;
-        this.sortForHttp.sortType = sort.sortType;
-        this.sortForHttp.priority = sort.priority;
-      },
-      editLabel(label) {
-        this.labelDialog = true;
-        this.labelForHttp.id = label.id;
-        this.labelForHttp.sortId = label.sortId;
-        this.labelForHttp.labelName = label.labelName;
-        this.labelForHttp.labelDescription = label.labelDescription;
-      },
-      insertLabel(sort) {
-        this.labelForHttp.sortId = sort.id;
-        this.labelDialog = true;
-      },
-      handleClose() {
-        this.labelForHttp = {
-          id: null,
-          sortId: null,
-          labelName: "",
-          labelDescription: ""
-        };
-        this.sortForHttp = {
-          id: null,
-          sortName: "",
-          sortDescription: "",
-          sortType: null,
-          priority: null
-        };
-        this.sortDialog = false;
-        this.labelDialog = false;
-      },
-      sayLabel(sort) {
-        this.sort = sort;
-      },
-      getSortInfo() {
-        this.$http.get(this.$constant.baseURL + "/webInfo/getSortInfo")
-          .then((res) => {
-            if (!this.$common.isEmpty(res.data)) {
-              this.sortInfo = res.data;
-            }
-          })
-          .catch((error) => {
-            this.$message({
-              message: error.message,
-              type: "error"
-            });
-          });
+      labelForHttp: {
+        id: null,
+        categoryId: null,
+        name: "",
+        description: ""
       }
     }
+  },
+  computed: {},
+  watch: {},
+  created() {
+    this.getCategories();
+  },
+  methods: {
+    deleteHandle(id, flag) {
+      let url;
+      if (flag === 1) {
+        url = "/web/category/" + id;
+      } else if (flag === 2) {
+        url = "/web/label/" + id;
+      } else {
+        return;
+      }
+      this.$confirm('确认删除？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.delete(this.$constant.baseURL + url)
+          .then((res) => {
+            this.$message({
+              message: "删除成功！",
+              type: "success"
+            });
+            this.getCategories();
+            this.category = {};
+          })
+          .catch((error) => {
+            this.$message({
+              message: error.message,
+              type: "error"
+            });
+          });
+      }).catch(() => {
+        this.$message({
+          type: 'success',
+          message: '已取消删除!'
+        });
+      });
+    },
+    saveEdit() {
+      if (this.$common.isEmpty(this.categoryForHttp.type) ||
+        this.$common.isEmpty(this.categoryForHttp.priority) ||
+        this.$common.isEmpty(this.categoryForHttp.name) ||
+        this.$common.isEmpty(this.categoryForHttp.description)) {
+        this.$message({
+          message: "请完善所有分类信息！",
+          type: "error"
+        });
+        return;
+      }
+
+
+      let httpMethod = null
+      if (this.$common.isEmpty(this.categoryForHttp.id)) {
+        httpMethod = this.$http.post(this.$constant.baseURL + "/web/category", this.categoryForHttp, true)
+      } else {
+        httpMethod = this.$http.patch(this.$constant.baseURL + "/web/category/" + this.categoryForHttp.id, this.categoryForHttp, true)
+      }
+
+      httpMethod.then((res) => {
+        this.$message({
+          message: "保存成功！",
+          type: "success"
+        });
+        this.getCategories();
+        this.handleClose();
+      })
+        .catch((error) => {
+          this.$message({
+            message: error.message,
+            type: "error"
+          });
+        });
+    },
+    saveLabelEdit() {
+      if (this.$common.isEmpty(this.labelForHttp.name) ||
+        this.$common.isEmpty(this.labelForHttp.description)) {
+        this.$message({
+          message: "请完善所有标签信息！",
+          type: "error"
+        });
+        return;
+      }
+
+      let httpMethod = null
+      if (this.$common.isEmpty(this.labelForHttp.id)) {
+        httpMethod = this.$http.post(this.$constant.baseURL + "/web/label", this.labelForHttp, true)
+      } else {
+        httpMethod = this.$http.patch(this.$constant.baseURL + "/web/label/" + this.labelForHttp.id, this.labelForHttp, true)
+      }
+
+      httpMethod.then((res) => {
+        this.$message({
+          message: "保存成功！",
+          type: "success"
+        });
+        this.getCategories();
+        this.handleClose();
+        this.category = {};
+      })
+        .catch((error) => {
+          this.$message({
+            message: error.message,
+            type: "error"
+          });
+        });
+    },
+    editCategory(category) {
+      this.categoryDialog = true;
+      this.categoryForHttp.id = category.id;
+      this.categoryForHttp.name = category.name;
+      this.categoryForHttp.description = category.description;
+      this.categoryForHttp.type = category.type;
+      this.categoryForHttp.priority = category.priority;
+    },
+    editLabel(label) {
+      this.labelDialog = true;
+      this.labelForHttp.id = label.id;
+      this.labelForHttp.categoryId = label.categoryId;
+      this.labelForHttp.name = label.name;
+      this.labelForHttp.description = label.description;
+    },
+    insertLabel(category) {
+      this.labelForHttp.categoryId = category.id;
+      this.labelDialog = true;
+    },
+    handleClose() {
+      this.labelForHttp = {
+        id: null,
+        categoryId: null,
+        name: "",
+        description: ""
+      };
+      this.categoryForHttp = {
+        id: null,
+        name: "",
+        description: "",
+        type: null,
+        priority: null
+      };
+      this.categoryDialog = false;
+      this.labelDialog = false;
+    },
+    sayLabel(category) {
+      this.category = category;
+    },
+    getCategories() {
+      this.$http.get(this.$constant.baseURL + "/web-categories")
+        .then((res) => {
+          if (!this.$common.isEmpty(res.data)) {
+            this.categories = res.data;
+          }
+        })
+        .catch((error) => {
+          this.$message({
+            message: error.message,
+            type: "error"
+          });
+        });
+    }
   }
+}
 </script>
 
 <style scoped>
 
-  .my-dialog > div {
-    margin: 12px;
-  }
+.my-dialog > div {
+  margin: 12px;
+}
 
-  .my-dialog >>> input::-webkit-inner-spin-button {
-    appearance: none;
-  }
+.my-dialog >>> input::-webkit-inner-spin-button {
+  appearance: none;
+}
 </style>
--- src/utils/request.js
+++ src/utils/request.js
@@ -10,10 +10,10 @@ axios.defaults.baseURL = constant.baseURL;
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
   // 已登录的情况下请求 api 增加 token 头
-  if(localStorage.getItem("adminToken") || localStorage.getItem("userToken")) {
+  if (localStorage.getItem("adminToken") || localStorage.getItem("userToken")) {
     let token = localStorage.getItem("adminToken")
       ? localStorage.getItem("adminToken") : localStorage.getItem("userToken")
-    config.headers.common['Authorization'] =  "Bearer " + token
+    config.headers.common['Authorization'] = "Bearer " + token
   }
   return config;
 }, function (error) {
@@ -22,35 +22,40 @@ axios.interceptors.request.use(function (config) {
 
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
-  if (response !== null && response.hasOwnProperty("status") && [200, 201].indexOf(response.status) < 0) {
-    if (response.status === 300) {
-      store.commit("loadCurrentUser", {});
-      if(localStorage.getItem("userToken")) {
-        localStorage.removeItem("userToken");
+    if (response !== null && response.hasOwnProperty("status") && [200, 201, 204].indexOf(response.status) < 0) {
+      if (response.status === 300) {
+        store.commit("loadCurrentUser", {});
+        if (localStorage.getItem("userToken")) {
+          localStorage.removeItem("userToken");
+        }
+        store.commit("loadCurrentAdmin", {});
+        if (localStorage.getItem("adminToken")) {
+          localStorage.removeItem("adminToken");
+        }
+        window.location.href = constant.webURL + "/user";
       }
-      store.commit("loadCurrentAdmin", {});
-      if(localStorage.getItem("adminToken")) {
-        localStorage.removeItem("adminToken");
+      return Promise.reject(new Error(response.data.message));
+    } else {
+      if (response != null) {
+        // 如果Headers中存在Authorization字段值就将新的 token 进行保存
+        if (response.headers.hasOwnProperty("authorization")) {
+          let newToken = response.headers.authorization.replace("Bearer", '').trim()
+          if (localStorage.getItem("userToken")) {
+            localStorage.setItem("userToken", newToken);
+          } else if (localStorage.getItem("adminToken")) {
+            localStorage.setItem("adminToken", newToken);
+          }
+        }
+        return response;
       }
-      window.location.href = constant.webURL + "/user";
     }
-    return Promise.reject(new Error(response.data.message));
-  } else {
-    // 如果Headers中存在Authorization字段值就将新的 token 进行保存
-    if(response != null && response.headers.hasOwnProperty("authorization")) {
-      let newToken = response.headers.authorization.replace("Bearer", '').trim()
-      if(localStorage.getItem("userToken")) {
-        localStorage.setItem("userToken", newToken);
-      } else if(localStorage.getItem("adminToken")) {
-        localStorage.setItem("adminToken", newToken);
-      }
-    }
-    return response;
+  },
+
+  function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error.response.data);
   }
-}, function (error) {
-  // 对响应错误做点什么
-  return Promise.reject(error.response.data);
-});
+);
 
 // 当data为URLSearchParams对象时设置为application/x-www-form-urlencoded;charset=utf-8
 // 当data为普通对象时，会被设置为application/json;charset=utf-8
@@ -95,11 +100,23 @@ export default {
     });
   },
 
+  delete(url, params = {}, isAdmin = false) {
+    return new Promise((resolve, reject) => {
+      axios.delete(url, {
+        params: params,
+      }).then(res => {
+        resolve(res.data);
+      }).catch(err => {
+        reject(err)
+      })
+    });
+  },
+
   upload(url, param, isAdmin = false) {
     let config;
 
     config = {
-      headers: { "Content-Type": "multipart/form-data"}
+      headers: {"Content-Type": "multipart/form-data"}
     };
     return new Promise((resolve, reject) => {
       axios
